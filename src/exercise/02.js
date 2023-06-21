90f159356486fe24085836b6d7db56faaa63a6c1
Cypress Frankenfeld <cypress@lumafield.com>
2023-06-21 17:04:33 -0500

    Exercise 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,31 +3,68 @@
 
 import * as React from 'react'
 
+function useLocalStorage(
+  key,
+  defaultValue,
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(
+    () =>
+      deserialize(window.localStorage.getItem(key)) ??
+      (typeof defaultValue === 'function' ? defaultValue() : defaultValue),
+  )
+  const previousKeyRef = React.useRef(key)
+  React.useEffect(() => {
+    if (previousKeyRef.current !== key) {
+      window.localStorage.removeItem(previousKeyRef.current)
+      previousKeyRef.current = key
+    }
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [key, setKey] = React.useState('name')
+  const [name, setName] = useLocalStorage(key, initialName)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
-
-  function handleChange(event) {
+  function handleNameChange(event) {
     setName(event.target.value)
+  }
+  function handleKeyChange(event) {
+    setKey(event.target.value)
   }
   return (
     <div>
       <form>
+        <label htmlFor="name-key">Key: </label>
+        <input value={key} onChange={handleKeyChange} id="name-key" />
+
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleNameChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
+function Counter() {
+  const [count, setCount] = useLocalStorage('count', 0)
+
+  return (
+    <button type="button" value={count} onClick={() => setCount(count + 1)}>
+      {count}
+    </button>
+  )
+}
+
 function App() {
-  return <Greeting />
+  return (
+    <>
+      <Counter />
+      <Greeting />
+    </>
+  )
 }
 
 export default App
